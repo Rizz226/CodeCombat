44c405d44014c855a5e5574485938965cc51e7e3
Rizz226 <74420891+Rizz226@users.noreply.github.com>
2022-05-27 08:29:54 -0400

    Create defineEnemies.py

---
+++ defineEnemies.py
@@ -0,0 +1,19 @@
+# All the enemy types I have come across
+
+enemies = self.findEnemies()
+enemy = self.findNearest(enemies)
+fangriders = self.findByType("fangrider")
+fangrider = self.findNearest(fangriders)
+throwers = self.findByType("thrower")
+thrower = self.findNearest(throwers)
+shamans = self.findByType("shaman")
+shaman = self.findNearest(shamans)   
+ogres = self.findByType("ogre")
+ogre = self.findNearest(ogres)
+munchkins = self.findByType("munchkin")
+munchkin = self.findNearest(munchkins)
+scouts = self.findByType("scout")
+scout = self.findNearest(scouts)
+
+# Fences and many doors
+other = undefined == 0 == null
