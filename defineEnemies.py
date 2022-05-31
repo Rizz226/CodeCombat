80a11f333109588995198918fe3a0d2a56eab49e
Rizz226 <74420891+Rizz226@users.noreply.github.com>
2022-05-31 07:48:32 -0400

    Update defineEnemies.py

--- defineEnemies.py
+++ defineEnemies.py
@@ -14,6 +14,9 @@ munchkins = self.findByType("munchkin")
 munchkin = self.findNearest(munchkins)
 scouts = self.findByType("scout")
 scout = self.findNearest(scouts)
-
+yaks = hero.findByType("yak")
+yak = hero.findNearest(yaks)
+sYaks = hero.findByType("sand-yak")
+sYak = hero.findNearest(sYaks)
 # Fences and many doors
 other = undefined == 0 == null
