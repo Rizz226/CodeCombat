5a0ec7494583b1eb2b6636d110ed2a1eaf37149c
Rizz226 <74420891+Rizz226@users.noreply.github.com>
2022-05-31 11:01:13 -0400

    Update usefulFunctions.py
    Added search function for prioritizing gold pickups.

--- usefulFunctions.py
+++ usefulFunctions.py
@@ -17,7 +17,8 @@ def cleaveCrowd:
         
 # this is essentially the same as the last one, but if you dont have cleave then drop a 
 # black flag outside of the fighting and your hero will rally to it when surrounded.
-def crowdCheck():
+def retreatFlag():
+    bFlag = hero.findFlag("black")
     tik = 0
     close = hero.findEnemies()
     for c in close:
@@ -28,3 +29,17 @@ def crowdCheck():
         trigger = 0
         hero.pickUpFlag(bFlag)
         
+
+# This is for prioritized  searching & looting. it grabs the nearest item but scans
+# for anything with a value greater than two.
+def search()
+    if item:
+        it = hero.findItems()
+        x = item.pos.x
+        y = item.pos.y
+        for i in it:
+            dis = hero.distanceTo(i)
+            a = i.pos.x
+            b = i.pos.y
+            if i.value > 2 and dis < 5:
+                hero.moveXY(a, b)
